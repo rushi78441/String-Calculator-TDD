5a65e4a04cc9325f69d1988223960f4cf527ef8a
rushi78441 <rushi78441@gmail.com>
2026-07-12 13:51:45 +0530

    Test : Adding single number (Red Green Refactor)

--- string_calculator/calculator.py
+++ string_calculator/calculator.py
@@ -1,3 +1,7 @@
 def add(numbers : str) -> int:
+    # case 1 : Empty string
     if not numbers:
         return 0
+
+    # case 2 : Adding Single Number
+    return int(numbers)
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,5 +1,11 @@
 import pytest
 from string_calculator.calculator import add
 
+# Empty String test
 def test_add_empty_string_return_zero():
     assert add("") == 0 , "String should be empty"
+
+# Single number test
+def test_add_single_number():
+    assert add("5") == 5, "single number 5 should be added"
+    assert add("10") == 10, "single number 10 should be added"
